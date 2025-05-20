3a7dff99e619d415f41c612faadd4f1302382527
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-05-20 19:01:41 +0300

    Обновлено обновление TaskItem в соответсвии с новой моделью TaskItem

--- ToDo.Application/TaskItems/Commands/UpdateTaskItem/UpdateTaskItemCommandValidator.cs
+++ ToDo.Application/TaskItems/Commands/UpdateTaskItem/UpdateTaskItemCommandValidator.cs
@@ -6,9 +6,34 @@ namespace ToDo.Application.TaskItems.Commands.UpdateTaskItem
     {
         public UpdateTaskItemCommandValidator()
         {
-            RuleFor(command => command.Id).NotEmpty();
-            RuleFor(command => command.UserId).NotEmpty();
-            RuleFor(command => command.Title).NotEmpty().MaximumLength(250);
+            RuleFor(cmd => cmd.Id)
+                .NotEmpty();
+
+            RuleFor(cmd => cmd.UserId)
+                .NotEmpty();
+
+            RuleFor(cmd => cmd.Title)
+                .NotEmpty()
+                .MaximumLength(250);
+
+            RuleFor(cmd => cmd.Description)
+                .MaximumLength(1000);
+
+            RuleFor(cmd => cmd.StartDate)
+                .LessThan(cmd => cmd.EndDate)
+                .WithMessage("'StartDate' must be earlier than 'EndDate'.");
+
+            RuleFor(cmd => cmd.EndDate)
+                .GreaterThan(cmd => cmd.StartDate)
+                .WithMessage("'EndDate' must be later than 'StartDate'.");
+
+            RuleFor(cmd => cmd.Color)
+                .MaximumLength(20);
+
+            RuleFor(cmd => cmd.RecurrenceRule)
+                .NotEmpty()
+                .When(cmd => cmd.IsRecurring)
+                .WithMessage("'RecurrenceRule' must be specified when 'IsRecurring' is true.");
         }
     }
 }
