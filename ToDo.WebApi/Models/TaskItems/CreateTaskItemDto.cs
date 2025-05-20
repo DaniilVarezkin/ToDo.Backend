3dcdead33c4a9397336b1ca81b422f904954621e
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-05-20 19:22:00 +0300

    Обновлены dto создания и обновления задачи в уровне Presentation в соответсвии с новой моделью  TaskItem

--- ToDo.WebApi/Models/TaskItems/CreateTaskItemDto.cs
+++ ToDo.WebApi/Models/TaskItems/CreateTaskItemDto.cs
@@ -1,28 +1,56 @@
 ﻿using AutoMapper;
+using System.ComponentModel.DataAnnotations;
 using ToDo.Application.Common.Mapping;
 using ToDo.Application.TaskItems.Commands.CreateTaskItem;
 using ToDo.Domain.Enums;
+using ToDo.Domain.Models;
 
 namespace ToDo.WebApi.Models.TaskItems
 {
+    /// <summary>
+    /// DTO для создания новой задачи
+    /// </summary>
     public class CreateTaskItemDto : IMapped
     {
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-        public required DateTime DueDate { get; set; }
-        public required UserTaskStatus Status { get; set; }
+        [Required, MaxLength(250)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public bool IsAllDay { get; set; } = false;
+
+        [Required]
+        public DateTimeOffset StartDate { get; set; }
+
+        [Required]
+        public DateTimeOffset EndDate { get; set; }
+
+        [MaxLength(20)]
+        public string? Color { get; set; }
+
+        public bool IsRecurring { get; set; } = false;
+
+        public string? RecurrenceRule { get; set; }
+
+        public UserTaskStatus Status { get; set; } = UserTaskStatus.None;
+
+        public TaskPriority Priority { get; set; } = TaskPriority.Medium;
 
         public void ConfigureMapping(Profile profile)
         {
             profile.CreateMap<CreateTaskItemDto, CreateTaskItemCommand>()
-                .ForMember(command => command.Title, opt =>
-                    opt.MapFrom(dto => dto.Title))
-                .ForMember(command => command.Description, opt =>
-                    opt.MapFrom(dto => dto.Description))
-                .ForMember(command => command.DueDate, opt =>
-                    opt.MapFrom(dto => dto.DueDate))
-                .ForMember(command => command.Status, opt =>
-                    opt.MapFrom(dto => dto.Status));
+                // Основные поля
+                .ForMember(cmd => cmd.Title, opt => opt.MapFrom(dto => dto.Title))
+                .ForMember(cmd => cmd.Description, opt => opt.MapFrom(dto => dto.Description))
+                .ForMember(cmd => cmd.IsAllDay, opt => opt.MapFrom(dto => dto.IsAllDay))
+                .ForMember(cmd => cmd.StartDate, opt => opt.MapFrom(dto => dto.StartDate))
+                .ForMember(cmd => cmd.EndDate, opt => opt.MapFrom(dto => dto.EndDate))
+                .ForMember(cmd => cmd.Color, opt => opt.MapFrom(dto => dto.Color))
+                .ForMember(cmd => cmd.IsRecurring, opt => opt.MapFrom(dto => dto.IsRecurring))
+                .ForMember(cmd => cmd.RecurrenceRule, opt => opt.MapFrom(dto => dto.RecurrenceRule))
+                .ForMember(cmd => cmd.Status, opt => opt.MapFrom(dto => dto.Status))
+                .ForMember(cmd => cmd.Priority, opt => opt.MapFrom(dto => dto.Priority));
         }
     }
 }
